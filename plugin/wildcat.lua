560ff4add9b5dbf425706d8ac30037b27f93a8ac
System Malt <javierorfo@protonmail.com>
2023-03-26 12:36:47 -0300

    Update wildcat.lua

--- plugin/wildcat.lua
+++ plugin/wildcat.lua
@@ -1,7 +1,7 @@
--- ########################################################
--- # Maintainer: Javier Orfo                              #
--- # URL:        https://github.com/javiorfo/nvim-wildcat #
--- ########################################################
+-- ##########################################################
+-- # Maintainer: System Malt                                #
+-- # URL:        https://github.com/systemmalt/nvim-wildcat #
+-- ##########################################################
 
 if vim.g.wildcat then
     return
