b6d8ee41ca79e6fa73a3991b15d6fe215bdbd44e
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:27:03 -0300

    Update wildcat.lua

--- lua/wildcat.lua
+++ lua/wildcat.lua
@@ -1,8 +1,3 @@
--- #########################################################
--- # Maintainer: Mr. Charkuils                             #
--- # URL:        https://github.com/charkuils/nvim-wildcat #
--- #########################################################
-
 local util = require'wildcat.util'
 local Logger = util.Logger
 
